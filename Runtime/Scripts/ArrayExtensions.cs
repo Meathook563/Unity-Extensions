abaec64c17fe92b9059fda9e7ae132c3c45f1bc3
Meathook563 <78488173+Meathook563@users.noreply.github.com>
2022-11-28 20:40:53 +0000

    Added Random Vector extensions and Random list & array elements

---
+++ Runtime/Scripts/ArrayExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace JustSomeExtenions
+{
+    public static class ArrayExtensions
+    {
+        
+    }
+}
+
+
---
+++ Runtime/Scripts/ListExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace Extension
+{
+    public static class ListExtensions
+    {
+        public static T GetRandom<T>(this T[] list)
+        {
+            return list[Random.Range(0, list.Length)];
+        }
+    }
+}
+
+
