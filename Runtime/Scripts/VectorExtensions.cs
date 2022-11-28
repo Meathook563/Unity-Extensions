90b66aaebc5fbc74c0c0c523e3b656bf4612e0f4
Meathook563 <78488173+Meathook563@users.noreply.github.com>
2022-11-28 20:46:41 +0000

    Added Random Vectors

--- Runtime/Scripts/VectorExtensions.cs
+++ Runtime/Scripts/VectorExtensions.cs
@@ -49,6 +49,49 @@ namespace JustSomeExtenions
 
             return minVector;
         }
+
+        //----------------------
+        // Random Vector Elements
+        //---------------------
+
+        public static Vector2 RandomVector2(this Vector2 vector)
+        {
+            return vector = new Vector2(Random.Range(0, 100),
+                                        Random.Range(0, 100));
+        }   
+
+        public static Vector2 RandomVector2(this Vector2 vector, float range)
+        {
+            return vector = new Vector2(Random.Range(-range, range),
+                               Random.Range(-range, range));
+        }
+
+        public static Vector2 RandomVector2(this Vector2 vector, Vector2 min, Vector2 max)
+        {
+            return vector = new Vector2(Random.Range(min.x, max.x),
+                                        Random.Range(min.y, max.y));
+        }
+
+        public static Vector3 RandomVector3(this Vector3 vector)
+        {
+            return vector = new Vector3(Random.Range(0, 100),
+                                        Random.Range(0, 100),
+                                        Random.Range(0, 100));
+        }
+
+        public static Vector3 RandomVector3(this Vector3 vector, float range)
+        {
+            return vector = new Vector3(Random.Range(0, range),
+                                        Random.Range(0, range),
+                                        Random.Range(0, range));
+        }
+
+        public static Vector3 RandomVector3(this Vector3 vector, Vector3 min, Vector3 max)
+        {
+            return vector = new Vector3(Random.Range(min.x, max.x),
+                                        Random.Range(min.y, max.y),
+                                        Random.Range(min.z, max.z));
+        }   
     }
 }
 
